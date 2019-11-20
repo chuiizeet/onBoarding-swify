a7d419e0f7431c03edcbcb250a6f8c6255bd003b
chuiizeet <chuy.guerra19@outlook.es>
2019-11-20 14:52:27 -0600

    Init files

---
+++ onboarding/onboarding/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  onboarding
+//
+//  Created by imac on 20/11/19.
+//  Copyright © 2019 JeguLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+    }
+
+
+}
+
